6a37aa0433434ad5de96ae2d61fbd96ee7473c8f
Duane Storey <duanestorey@gmail.com>
2024-12-22 19:59:58 -0800

    Updating to 1.0.4

--- juniper-author.php
+++ juniper-author.php
@@ -6,7 +6,7 @@
     Description: Facilitates code signing and releases for WordPress add-ons using the Juniper release system for WordPress
     Author: Duane Storey
     Author URI: https://duanestorey.com
-    Version: 1.0.3
+    Version: 1.0.4
     Requires PHP: 6.0
     Requires at least: 6.0
     Tested up to: 6.7
@@ -15,10 +15,10 @@
     Domain Path: /lang
     Primary Branch: main
     GitHub Plugin URI: duanestorey/juniper-author
-    Stable: 1.0.3
+    Stable: 1.0.4
     Authority: https://plugins.duanestorey.com
 
-    Copyright (C) 2024-025 by Duane Storey - All Rights Reserved
+    Copyright (C) 2024-2025 by Duane Storey - All Rights Reserved
     You may use, distribute and modify this code under the
     terms of the GPLv3 license.
 */
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-define( 'JUNIPER_AUTHOR_VER', '1.0.3' );
+define( 'JUNIPER_AUTHOR_VER', '1.0.4' );
 define( 'JUNIPER_AUTHOR_PATH', dirname( __FILE__ ) );
 define( 'JUNIPER_AUTHOR_RELEASES_PATH', dirname( __FILE__ ) . '/releases' );
 define( 'JUNIPER_AUTHOR_MAIN_FILE', __FILE__ );
