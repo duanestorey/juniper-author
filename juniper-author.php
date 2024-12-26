a1d9c94f22cd82a078ea7be392fc02d8604c24db
Duane Storey <duanestorey@gmail.com>
2024-12-25 17:19:10 -0800

    Updating to 1.1.1

--- juniper-author.php
+++ juniper-author.php
@@ -15,7 +15,7 @@
     Domain Path: /lang
     Primary Branch: main
     GitHub Plugin URI: duanestorey/juniper-author
-    Stable: 1.1.0
+    Stable: 1.1.1
     Authority: https://plugins.duanestorey.com
 
     Copyright (C) 2024-2025 by Duane Storey - All Rights Reserved
