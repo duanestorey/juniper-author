c339e1948f59efc2e22b8aabb8005ec19f030c71
Duane Storey <duanestorey@gmail.com>
2024-12-27 17:19:10 -0800

    Updates to js

--- dist/juniper-author.js
+++ dist/juniper-author.js
@@ -315,7 +315,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkjuniper_author"] = self["webpackChunkjuniper_author"] || [];
+/******/ 		var chunkLoadingGlobal = globalThis["webpackChunkjuniper_author"] = globalThis["webpackChunkjuniper_author"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
